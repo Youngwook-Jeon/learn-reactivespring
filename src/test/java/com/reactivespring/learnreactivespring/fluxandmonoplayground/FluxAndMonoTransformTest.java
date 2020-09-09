f330a9468f645b43a3ed087420f6f8fdec6eaec4
Young wook Jeon <ela87@naver.com>
2020-09-09 15:29:29 +0900

    Add Transform/Filter tests on Flux and Mono.

---
+++ src/test/java/com/reactivespring/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -0,0 +1,99 @@
+package com.reactivespring.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static reactor.core.scheduler.Schedulers.parallel;
+
+public class FluxAndMonoTransformTest {
+
+    List<String> names = Arrays.asList("adam", "anna", "youngwook", "mayer");
+
+    @Test
+    public void transformUsingMap() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .map(String::toUpperCase)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("ADAM", "ANNA", "YOUNGWOOK", "MAYER")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingMap_Length_Repeat() {
+        Flux<Integer> namesFlux = Flux.fromIterable(names)
+                .map(String::length)
+                .repeat(1)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext(4, 4, 9, 5, 4, 4, 9, 5)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingMap_Filter() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .filter(s -> s.length() > 4)
+                .map(s -> s.toUpperCase())
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("YOUNGWOOK", "MAYER")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingFlatMap() {
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                .flatMap(s -> Flux.fromIterable(convertToList(s)))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    private List<String> convertToList(String s) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "newValue");
+    }
+
+    @Test
+    public void transformUsingFlatMap_Using_Parallel() {
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                .window(2)
+                .flatMap(s -> s.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(Flux::fromIterable)
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap_Using_Parallel_Maintaining_Order() {
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                .window(2)
+                .flatMapSequential(s -> s.map(this::convertToList).subscribeOn(parallel()))
+                .flatMap(Flux::fromIterable)
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+}
